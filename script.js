49cddb5621d0de0ae1b842c20234a26cc0c8ceb1
Nathan-0001 <nathan.hartshorn24@gmail.com>
2025-07-31 20:32:10 +0100

    progressing further

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function hideSidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none';          
     }
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children)
+const nextButton = document.querySelector('.carousel__button--right') ;
+const prevButton = document.querySelector('.carousel__button--left');
+
+const slideSize = slides[0].getBoundingClientRect();
+console.log(slideSize);
+
+//When left is clicked, slides left
+//when right is clicked, slides right
