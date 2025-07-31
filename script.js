17918920ad411a0b25c6c356d45d648a9d888e80
Nathan-0001 <nathan.hartshorn24@gmail.com>
2025-07-31 21:43:44 +0100

    Carousel completion

--- script.js
+++ script.js
@@ -11,8 +11,50 @@ const slides = Array.from(track.children)
 const nextButton = document.querySelector('.carousel__button--right') ;
 const prevButton = document.querySelector('.carousel__button--left');
 
-const slideSize = slides[0].getBoundingClientRect();
-console.log(slideSize);
+const slideWidth = slides[0].getBoundingClientRect().width;
+console.log(slideWidth);
+//Arranging slides next to eachother
+const setSlidePosition = (slide, index) => {slide.style.left = slideWidth * index + 'px';}
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left ;
+    //Change current slide to next
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+
+    
+}
 
 //When left is clicked, slides left
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide)
+
+    moveToSlide(track, currentSlide, prevSlide);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex);
+})
 //when right is clicked, slides right
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide)
+
+    moveToSlide(track, currentSlide, nextSlide);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
+})
+// remove buttons when at start/end
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+    if (targetIndex === 0) {
+    prevButton.classList.add('is-hidden')
+    nextButton.classList.remove('is-hidden');
+}else if (targetIndex === slides.length - 1) {
+    nextButton.classList.add('is-hidden')
+    prevButton.classList.remove('is-hidden');
+}
+else {
+    nextButton.classList.remove('is-hidden')
+    prevButton.classList.remove('is-hidden');
+}
+}
